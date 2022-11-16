104ed2cbcf07366ec3adc43acfae0bc942cc4730
DarsStas <darstas@yahoo.com>
2022-11-16 21:13:03 +0200

    Aktyvumas

--- app.js
+++ app.js
@@ -1,33 +1,47 @@
-/*
+/* [20:37] Vytautas Stripeika
+2. Sukurti BMR kalkuliatorių
 
-  Formula:
-
-
-
-  weight (kg) / [height (m)]2
-
-  Or
+For men: BMR = 66.5 + (13.75 * weight in kg) + (5.003 * height in cm) - (6.75 * age)
+For women: BMR = 655.1 + (9.563 * weight in kg) + (1.850 * height in cm) - (4.676 * age)
 
 
+To determine your total daily calorie needs, multiply your BMR by the appropriate activity factor, as follows:
+Sedentary (little or no exercise): calories = BMR × 1.2;
+Lightly active (light exercise/sports 1-3 days/week): calories = BMR × 1.375;
+Moderately active (moderate exercise/sports 3-5 days/week): calories = BMR × 1.55;
+Very active (hard exercise/sports 6-7 days a week): calories = BMR × 1.725; and
+If you are extra active (very hard exercise/sports & a physical job): calories = BMR × 1.9.
 
-  [weight (kg) / height (cm) / height (cm)] x 10,000
+// suvedami atsakymai ar esam vyras/moteris, svoris aukštis ir amžius.
+// atsakomas koks mūsų aktyvumo lygis
+// atsakomas kiek kalorijų mums reikia per dieną */
 
-*/
+const gender = prompt("Jūsų lytis (vyras, moteris)");
+const weight = Number(prompt("Jūsų svoris (kg)"));
+const height = Number(prompt("Jūsų ūgis (cm)"));
+const age = Number(prompt("Jūsų amžius (metai)"));
+const activityLevel = Number(prompt("Jūsų aktyvumo lygis (0, 1, 2, 3, 4)"));
 
-const svoris = Number(prompt("Koks jūsų svoris (kg)?"));
-const ugisKv = Number(prompt("Koks jūsų ūgis (m)?"))**2;
+let BMR;
 
-const KMI = svoris / ugisKv
+if (gender == "vyras") {
+    BMR = (66.5 + (13.75 * weight) + (5.003 * height) - (6.75 * age));
+    console.log("vyras");
+}else {
+    BMR = (655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age));
+    console.log("moteris");
+}
 
-if (KMI < 18.5) {
-    alert("Under Weight");
-}else if (KMI < 25) {
-    alert("Normal");
-}else if (KMI < 30) {
-    alert("Over Weight");
-} else if (KMI < 35) {
-    alert("Obesity (Class I)");
-} else if (KMI < 40) {
-    alert("Obesity (Class II)");
-} else (KMI > 40) 
-    alert("Extreme Obesity");
+console.log(BMR);
+
+if (activityLevel === 0) {
+    alert(BMR * 1.2);
+} else if (activityLevel === 1) {
+    alert(BMR * 1.375);
+} else if (activityLevel === 2) {
+    alert(BMR * 1.55);
+}  else if (activityLevel === 3) {
+    alert(BMR * 1.725);
+} else {
+    alert(BMR * 1.9);
+}
