7a5ccaa7a594308fe52ea650eb475da43c1e4cf0
DarsStas <darstas@yahoo.com>
2022-11-21 20:25:02 +0200

    Button i + 1

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-// const heading = document.querySelector("#my-element > h1");
+const button = document.querySelector("#my-button");
 
-// // heading.className stringas su klasem
-// // heading.innerText stringas su tekstu tarp <el> </el>
+let i = 0;
 
-// heading.innerText = "Įvyko klaida!!!!"
-// console.log(heading.innerText);
-
-//            FUNKCIJOS
-
-if(true) {
-  console.log("true");
-}
-
-// deklaraciija
 function myFunction() {
-  console.log("function");
+  i++ // i = i + 1
+  button.innerText = i;
 }
-
-// console.log("1");
-// //panaudojimas
-// myFunction(); // funkcijos iškvietimas | reikšmes pasiima iš deklaracijos
-// console.log("2");
-// myFunction(); // reiksme jau nurodyta ir ji visada bus ta pati jei skliaustai tusti
-// myFunction();
-// myFunction();
-
-// // testukas
-
-// function manoFunkcija () {
-//   console.log("testukas");
-// }
-
-// manoFunkcija();
-// manoFunkcija();
-// console.log("rodo, kad funkcija yra paleidziama, ir veikia kaip log");
