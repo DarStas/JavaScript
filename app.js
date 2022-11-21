96abab082ad6a342f4b948b8b17512383dc36d68
DarsStas <darstas@yahoo.com>
2022-11-21 20:55:15 +0200

    Dropdown active or not

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-const button = document.querySelector("#my-button");
+// const button = document.querySelector("#my-button");
 
-let i = 0;
+// let i = 0;
 
-function myFunction() {
-  i++ // i = i + 1
-  button.innerText = i;
+// function myFunction() {
+//   i++ // i = i + 1
+//   button.innerText = i;
+// }
+
+const DROPDOWN_ACTIVE = "dropdown-menu active";
+const DROPDOWN_HIDDEN = "dropdown-menu";
+
+const dropdownElement = document.querySelector(`#dropdown .${DROPDOWN_HIDDEN}`);
+
+/**
+ * atvejis 1:
+ * jeigu dropdown-menu yra active, tai jis turi issijungti
+ * 
+ * atvejis 2:
+ * jei dropdown-menu nera active, tai jis turi isijungti
+ */
+
+function toggle() {
+  if (dropdownElement.className === DROPDOWN_ACTIVE) {
+    dropdownElement.className = DROPDOWN_HIDDEN;
+  } else {
+    dropdownElement.className = DROPDOWN_ACTIVE;
+  }
 }
