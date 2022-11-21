baf8f9d092daf080183fab7b4881a54e84cd262c
DarsStas <darstas@yahoo.com>
2022-11-21 20:06:41 +0200

    3 uzuotis

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
-const heading = document.querySelector("#my-element > h1");
+// const heading = document.querySelector("#my-element > h1");
 
-// heading.className stringas su klasem
-// heading.innerText stringas su tekstu tarp <el> </el>
-heading.innerText = "Įvyko klaida!!!!"
-console.log(heading.innerText);
+// // heading.className stringas su klasem
+// // heading.innerText stringas su tekstu tarp <el> </el>
+
+// heading.innerText = "Įvyko klaida!!!!"
+// console.log(heading.innerText);
+
+//            FUNKCIJOS
+
+if(true) {
+  console.log("true");
+}
+
+// deklaraciija
+function myFunction() {
+  console.log("function");
+}
+
+// console.log("1");
+// //panaudojimas
+// myFunction(); // funkcijos iškvietimas | reikšmes pasiima iš deklaracijos
+// console.log("2");
+// myFunction(); // reiksme jau nurodyta ir ji visada bus ta pati jei skliaustai tusti
+// myFunction();
+// myFunction();
+
+// // testukas
+
+// function manoFunkcija () {
+//   console.log("testukas");
+// }
+
+// manoFunkcija();
+// manoFunkcija();
+// console.log("rodo, kad funkcija yra paleidziama, ir veikia kaip log");
